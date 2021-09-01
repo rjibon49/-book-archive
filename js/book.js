349eaba38b3064eaac7bf7cc072f71760c1a9dd4
Md.  Raihan Hossain Jibon <53583320+rjibon49@users.noreply.github.com>
2021-09-02 02:11:52 +0600

    updatebook

--- js/book.js
+++ js/book.js
@@ -1,6 +1,17 @@
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
+    // console.log(searchText);
     searchField.value = '';
+    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    // console.log(url);
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayBook(data));
+}
+
+const displayBook = data => {
+    const books = data.docs;
+    console.log(data.docs);
 }
