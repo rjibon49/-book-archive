9997369df93e3324cfb0808e1bb6e2d69fc80224
Md.  Raihan Hossain Jibon <53583320+rjibon49@users.noreply.github.com>
2021-09-02 11:25:10 +0600

    update author

--- js/book.js
+++ js/book.js
@@ -8,10 +8,25 @@ const searchBook = () => {
 
     fetch(url)
     .then(res => res.json())
-    .then(data => displayBook(data));
+    .then(data => displayBookResalt(data.docs));
 }
 
-const displayBook = data => {
-    const books = data.docs;
-    console.log(data.docs);
+const displayBookResalt = docs => {
+    const searchResult = document.getElementById('search-result');
+    docs.forEach(docs => {
+        console.log(docs);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="${docs.cover_i}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${docs.title}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    });
+    
 }
