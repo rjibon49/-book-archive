c3f6fda81c246687969e00b8e5765529111490a6
Md.  Raihan Hossain Jibon <53583320+rjibon49@users.noreply.github.com>
2021-09-02 22:06:26 +0600

    update

--- js/book.js
+++ js/book.js
@@ -3,30 +3,46 @@ const searchBook = () => {
     const searchText = searchField.value;
     // console.log(searchText);
     searchField.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
     // console.log(url);
+
 
     fetch(url)
     .then(res => res.json())
     .then(data => displayBookResalt(data.docs));
+
 }
+
 
 const displayBookResalt = docs => {
     const searchResult = document.getElementById('search-result');
-    docs.forEach(docs => {
-        console.log(docs);
+    searchResult.innerHTML = '';
+
+    const result = document.getElementById('result');
+    let resultShow = result.innerText;
+    // result.innerText = docs.length;
+    // console.log(resultShow);
+
+ if (docs.length === 0) {
+    result.innerText = 'Serch Not Found ';
+ } else{
+    result.innerText = 'Search Found ' + docs.length;
+    docs.forEach(docs=>{
+        const cover = `https://covers.openlibrary.org/b/id/${docs.cover_i}-M.jpg`;
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
             <div class="card h-100">
-                <img src="${docs.cover_i}" class="card-img-top" alt="...">
+                <img src="${cover}" class="card-img-top" alt="${docs.cover_i}">
                 <div class="card-body">
                     <h5 class="card-title">${docs.title}</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
+                    <p class="card-text ">Author: ${docs.author_name}</p>
+                    <p class="card-text ">First publish year: ${docs.first_publish_year}</p>
             </div>
         `;
         searchResult.appendChild(div);
-    });
+    }) 
+ }
     
 }
